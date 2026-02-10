d75cc2312ea81199a7bfbb781a4c7dfe275ed6f3
N-SubhamGit <subhamnayak720@gmail.com>
2026-02-10 17:32:22 +0000

    Deploying to gh-pages from @ N-SubhamGit/N-SubhamGit.github.io@448b43c09da1185686ea92a2950bd34ab86575ae 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,8 +3,8 @@ const ninja = document.querySelector('ninja-keys');
 
 // add the home and posts menu items
 ninja.data = [{
-    id: "nav-subham-nayak",
-    title: "Subham Nayak",
+    id: "nav-about",
+    title: "About",
     section: "Navigation",
     handler: () => {
       window.location.href = "/";
