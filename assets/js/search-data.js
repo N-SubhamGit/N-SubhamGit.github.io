488261fc9a46d993e7a9d4985fe9b96003a8a596
N-SubhamGit <subhamnayak720@gmail.com>
2026-02-11 19:53:29 +0000

    Deploying to gh-pages from @ N-SubhamGit/N-SubhamGit.github.io@97cec148ec5f02c9e9b21716c7740058ca65a00d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -23,19 +23,19 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/publications/";
           },
-        },{id: "nav-education",
-          title: "Education",
-          description: "Academic background, qualifying examinations, and technical expertise.",
-          section: "Navigation",
-          handler: () => {
-            window.location.href = "/education/";
-          },
         },{id: "nav-activities",
           title: "Activities",
           description: "Conferences, workshops, and research engagements.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/activities/";
+          },
+        },{id: "nav-teaching",
+          title: "Teaching",
+          description: "Teaching Assistantship at IISER Thiruvananthapuram.",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/teaching/";
           },
         },{id: "nav-teaching",
           title: "Teaching",
